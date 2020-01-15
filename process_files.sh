21125b73ebaf6cb3946f04cd4c5adf20da3f2dc4
ilan-gold <ilanbassgold@gmail.com>
2020-01-15 15:52:35 -0500

    add echo of progress

--- process_files.sh
+++ process_files.sh
@@ -6,7 +6,10 @@ for FPATH in $(find /data/counts/*/*.counts); do
   PROJECT=$(echo $PROJECT_CELL |  cut -d '/' -f 1)
   # Get CELL
   CELL=$(echo $PROJECT_CELL |  cut -d '/' -f 2)
+  echo "Making Directory /data/gene_count_length_files/$PROJECT"
   mkdir -p /data/gene_count_length_files/$PROJECT
+  echo "Making file /data/gene_count_length_files/$PROJECT/$CELL.tsv"
   touch /data/gene_count_length_files/$PROJECT/$CELL.tsv
+  echo "Truncating /data/counts/$PROJECT/$CELL.counts columns"
   cut -f 1,6,7 /data/counts/$PROJECT/$CELL.counts > /data/gene_count_length_files/$PROJECT/$CELL.tsv
 done
