e09d76fbb61dfd9c12b6e0c7394451368cd34f87
ilan-gold <ilanbassgold@gmail.com>
2020-01-15 15:49:35 -0500

    change group to project

--- process_files.sh
+++ process_files.sh
@@ -3,9 +3,9 @@ for FPATH in $(find /data/counts/*/*.counts); do
   # cut off .counts ending and return the group cell combination
   PROJECT_CELL=$(echo $FPATH | cut -d '/' -f 4,5 | cut -d '.' -f 1)
   # Get specific GROUP
-  PROJECT=$(echo $GROUP_CELL |  cut -d '/' -f 1)
+  PROJECT=$(echo $PROJECT_CELL |  cut -d '/' -f 1)
   # Get CELL
-  CELL=$(echo $GROUP_CELL |  cut -d '/' -f 2)
+  CELL=$(echo $PROJECT_CELL |  cut -d '/' -f 2)
   mkdir -p /data/gene_count_length_files/$PROJECT
   touch /data/gene_count_length_files/$PROJECT/$CELL.tsv
   cut -f 1,6,7 /data/counts/$PROJECT/$CELL.counts > /data/gene_count_length_files/$PROJECT/$CELL.tsv
