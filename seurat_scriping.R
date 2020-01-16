ef913ee287afb470aff4721da16df33e47297cd1
ilan-gold <ilanbassgold@gmail.com>
2020-01-16 16:08:09 +0000

    Updated R script to load all data

--- seurat_scriping.R
+++ seurat_scriping.R
@@ -1,16 +1,29 @@
 library(Seurat)
-
-# get list of files with PROJECT/CELL.tsv
-rnaseq_counts_folder <- "/data/gene_count_length_files/"
-rna_seq_counts_files <- list.files(path=rnaseq_counts_folder, recursive=TRUE, pattern="*.tsv")
-full_df <- data.frame()
-
-for (file in rna_seq_counts_files) {
-  # read in each file
-  file_df <- read.table(file=paste(rnaseq_counts_folder, file, sep=''), sep="\t", header=TRUE, row.names='Geneid')
-  file_name <- strsplit(file, '/')[[1]][2]
-  cell <- strsplit(file_name, '.tsv')[[1]]
-  colnames(file_df) <- gsub(".*\\.bam", cell, colnames(file_df))
-  full_df <- merge(x=full_df, y=file_df, all=TRUE)
+library(dplyr)
+read_data <- function(){
+  # get list of files with PROJECT/CELL.tsv
+  rnaseq_counts_folder <- "/data/gene_count_length_files/"
+  rnaseq_counts_projects_names <- list.files(path=rnaseq_counts_folder)
+  rnaseq_counts_projects <- vector(mode="list", length=length(rnaseq_counts_projects_names))
+  names(rnaseq_counts_projects) <- rnaseq_counts_projects_names
   
+  for (project in rnaseq_counts_projects_names){
+    full_project_path <- paste(rnaseq_counts_folder, project, sep='')
+    project_cell_files <- list.files(path=full_project_path, recursive=TRUE, pattern="*.tsv")
+    full_project_df <- data.frame(matrix(ncol = 1, nrow = 0))
+    colnames(full_project_df) <- c('Geneid')
+    for (file in project_cell_files) {
+      # read in each file
+      file_df <- read.table(file=paste(full_project_path, file, sep='/'), sep="\t", header=TRUE)
+      cell <- strsplit(file, '.tsv')[[1]]
+      colnames(file_df) <- gsub(".*\\.bam", cell, colnames(file_df))
+      full_project_df <- merge(x=full_project_df, y=file_df, all=TRUE, by='Geneid')
+      full_project_df$Length <- coalesce(full_project_df$Length.x, full_project_df$Length.y)
+      full_project_df$Length.x <- NULL
+      full_project_df$Length.y <- NULL
+    }
+    rownames(full_project_df) <- full_project_df$Geneid
+    rnaseq_counts_projects$project <- full_project_df
+  }
 }
+
