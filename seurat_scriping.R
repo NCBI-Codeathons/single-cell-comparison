9f8a07a1e3c0c681a6dbce4e77357a8902ab93c5
ilan-gold <ilanbassgold@gmail.com>
2020-01-16 16:09:53 +0000

    Add comments

--- seurat_scriping.R
+++ seurat_scriping.R
@@ -1,6 +1,7 @@
 library(Seurat)
 library(dplyr)
 read_data <- function(){
+  
   # get list of files with PROJECT/CELL.tsv
   rnaseq_counts_folder <- "/data/gene_count_length_files/"
   rnaseq_counts_projects_names <- list.files(path=rnaseq_counts_folder)
@@ -8,22 +9,34 @@ read_data <- function(){
   names(rnaseq_counts_projects) <- rnaseq_counts_projects_names
   
   for (project in rnaseq_counts_projects_names){
+    
+    # get all cell files in project folder and create empty large df
     full_project_path <- paste(rnaseq_counts_folder, project, sep='')
     project_cell_files <- list.files(path=full_project_path, recursive=TRUE, pattern="*.tsv")
     full_project_df <- data.frame(matrix(ncol = 1, nrow = 0))
     colnames(full_project_df) <- c('Geneid')
+    
     for (file in project_cell_files) {
+      
       # read in each file
       file_df <- read.table(file=paste(full_project_path, file, sep='/'), sep="\t", header=TRUE)
+      
+      # alter column name of hits to be that of the cell
       cell <- strsplit(file, '.tsv')[[1]]
       colnames(file_df) <- gsub(".*\\.bam", cell, colnames(file_df))
+      
+      # merge into full dataframe for the project
       full_project_df <- merge(x=full_project_df, y=file_df, all=TRUE, by='Geneid')
       full_project_df$Length <- coalesce(full_project_df$Length.x, full_project_df$Length.y)
       full_project_df$Length.x <- NULL
       full_project_df$Length.y <- NULL
+      
     }
+    
+    #put the project into the project list
     rownames(full_project_df) <- full_project_df$Geneid
     rnaseq_counts_projects$project <- full_project_df
+    
   }
 }
 
